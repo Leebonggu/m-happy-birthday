01914810106d72f846b22ca8859d3c92f114b355
Leebonggu <leebonggu12@hanmail.net>
2023-01-13 02:14:54 +0900

    Remove unused code

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,4 @@
-import { useState } from 'react';
-import Button from '../components/Button';
-import Cake from '../components/Cake';
-import Card from '../components/Card';
-import Go from '../components/Travel';
+import Travel from '../components/Travel';
 
 export default function Home() {
   return <div>공사중</div>;
