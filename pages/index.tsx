31f52c45696caab9346a062c013d7d4c0a6c24a0
Leebonggu <leebonggu12@hanmail.net>
2023-01-13 00:32:10 +0900

    Remove API

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import Button from '../components/Button';
 import Cake from '../components/Cake';
 import Card from '../components/Card';
 
-export default function Home({ data }: any) {
+export default function Home() {
   const [gift, setGift] = useState(false);
   const handleGift = () => {
     setGift(true);
@@ -11,20 +11,10 @@ export default function Home({ data }: any) {
   return (
     <div className='w-full h-screen flex justify-center items-center bg-navy-100'>
       <div className='flex flex-col'>
-        <Card name={data.name} message={data.message} />
+        <Card name={''} message={''} />
         <Cake gift={gift} />
         {!gift ? <Button onClick={handleGift} /> : null}
       </div>
     </div>
   );
-}
-
-export async function getServerSideProps() {
-  const res = await fetch(
-    'https://6iwnip9feh.execute-api.ap-northeast-2.amazonaws.com/hodeug',
-  );
-  const { data } = await res.json();
-  return {
-    props: { data }, // will be passed to the page component as props
-  };
 }
