4bcc5104a0b67391fb6a52244492226301e807bf
Leebonggu <leebonggu12@hanmail.net>
2023-02-01 02:35:18 +0900

    Remove file-loader

--- next.config.js
+++ next.config.js
@@ -6,13 +6,6 @@ const nextConfig = {
     styledComponents: true,
   },
   webpack: (config) => {
-    // config.module.rules.push({
-    //   test: /\.(jpe?g|png|svg|gif|ico|eot|ttf|woff|woff2|mp4|mp3|pdf|webm)$/,
-    //   use: {
-    //     loader: 'file-loader',
-    //   },
-    // });
-
     config.module.rules.push({
       test: /\.(jpe?g|png|svg|gif|ico|eot|ttf|woff|woff2|mp4|mp3|pdf|webm)$/,
       use: {
